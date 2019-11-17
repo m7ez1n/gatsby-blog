7e673ec545d3b92f5b130868340113efe0e0ff2b
m7Aei <mateuspm2015@hotmail.com>
2019-11-17 16:06:13 -0300

    Fazendo a parte icones das redes sociais

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/m7Aei'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/m7He4rt'
+  },
+  {
+    label: 'Youtube',
+    url: 'https://www.youtube.com/'
+  },
+  { 
+    label: 'Instagram',
+    url: 'https://www.instagram.com/m7He4rt/'
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/m7aei/'
+  }
+]
+
+export default links;
