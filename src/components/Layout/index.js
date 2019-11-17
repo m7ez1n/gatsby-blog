70aa8722a6faa2c3b897dd3d4c81827d103419f7
m7Aei <mateuspm2015@hotmail.com>
2019-11-17 15:13:58 -0300

    Criando o componente do layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import GlobalStyles from '../../styles/global';
+import Sidebar from "../Sidebar"
+
+import * as S from './styles';
+
+const Layout = ({ children }) => {
+  return (
+    <S.LayoutWrapper>
+      <GlobalStyles />
+      <Sidebar />
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
