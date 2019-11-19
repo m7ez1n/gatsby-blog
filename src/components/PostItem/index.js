8451eb4c7226e378d9cfd91a5984d99f32287749
m7Aei <mateuspm2015@hotmail.com>
2019-11-19 09:49:00 -0300

    Fazendo algumas alterações no postItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,25 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import * as S from './styles'
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({ slug, background, category, date, timeToRead, title, description }) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>
+        {category}
+      </S.PostItemTag>
       <S.PostItemInfo>
         <S.PostItemDate>
-          30 de julho de 1029 | 4min de leitura
+          {date} | {timeToRead} min de leitura
         </S.PostItemDate>
         <S.PostItemTitle>
-          Vamo fazer um blog legal de javascripto
+          {title}
         </S.PostItemTitle>
         <S.PostItemDescription>
-          Uma descrição foda pra um post no blog que vai ficar bem
-          da hora pq a gente ta fazendo algo da hora
+          {description}
         </S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,15 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem 
+      slug="/about/"
+      background="red"
+      category="MISC"
+      date="19 de novembro de 2019"
+      timeToRead="5"
+      title="Post mo dahora de react e gatsby"
+      description="Algumas palavras aleatorias ai daquele modelo tlg?"
+    />
   </Layout>
 )
 
