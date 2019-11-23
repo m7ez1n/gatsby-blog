2c92c4c1a265936377bc0432f20e62e2980e83d2
m7Aei <mateuspm2015@hotmail.com>
2019-11-23 13:51:40 -0300

    Configurando o algolia para buscar o background

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const postQuery = `{
   posts: allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }){
     edges {
@@ -8,6 +10,7 @@ const postQuery = `{
         }
         frontmatter {
           title
+          background
           category
           date_timestamp: date
           date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
@@ -33,7 +36,7 @@ const queries = [
   {
     query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: `Posts`,
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
     settings,
   },
 ]
