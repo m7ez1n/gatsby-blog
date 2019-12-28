4c487e48a1bb680b719e86e3a75052a5dd336a26
m7Aei <mateuspm2015@hotmail.com>
2019-12-28 01:30:16 -0300

    :rocket: instalando e configurando o PWA e o SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     position: `Frontend Developer`,
     description: `A blog about frontend development and other cool stuff.`,
     author: `@m7He4rt`,
+    siteUrl: `https://m7-dev.com.br`,
   },
   plugins: [
     `gatsby-plugin-transition-link`,
@@ -41,9 +42,9 @@ module.exports = {
           {
             resolve: "gatsby-remark-relative-images",
             options: {
-              name: "uploads"
-            }
-          }, 
+              name: "uploads",
+            },
+          },
           {
             resolve: "gatsby-remark-images",
             options: {
@@ -52,7 +53,7 @@ module.exports = {
             },
           },
           `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`
+          `gatsby-remark-prismjs`,
         ],
       },
     },
@@ -72,17 +73,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mateus Mendes`,
+        short_name: `m7`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
